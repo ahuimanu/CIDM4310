983c947ad8bc102af6003a18008ab75cfa2e75cb
Jeffry Babb <ahuimanu@gmail.com>
2024-07-13 16:40:15 -0500

    added weather files

--- CuratedFlights/iem_query.py
+++ CuratedFlights/iem_query.py
@@ -13,31 +13,66 @@ Author: daryl herzmann akrherz@iastate.edu
 import os
 from datetime import date, datetime, timedelta
 
+import argparse
 import requests
 
+def configargs():
+    parser = argparse.ArgumentParser()
 
-def fetch(station_id):
+    # stations argument
+    parser.add_argument("--station", help="IEM ASOS station to download")
+    parser.add_argument("--start", help="Start date YYYYMMDD")
+    parser.add_argument("--end", help="End date YYYYMMDD")
+    args = parser.parse_args()
+    return args
+
+def validateargs(args):
+    if args.station is None:
+        print("Please provide a three-letter IEM ASOS station to download")
+        exit(1)
+
+    if args.start is None:
+        print("Please provide a Start date YYYYMMDD")
+        exit(1)        
+
+    if args.end is None:
+        print("Please provide an End date YYYYMMDD")
+        exit(1)          
+
+
+def fetch(station_id, start_date, end_date):
     """Download data we are interested in!"""
-    localfn = f"{station_id}.csv"
-    if os.path.isfile(localfn):
-        print(f"- Cowardly refusing to over-write existing file: {localfn}")
+    print(f"Downloading for {station_id} between {start_date} and {end_date}")
+    station_output_file = f"{station_id}.csv"
+    if os.path.isfile(station_output_file):
+        print(f"- Unable to over-write existing file: {station_output_file}")
         return
     print(f"+ Downloading for {station_id}")
-    enddt = date.today() + timedelta(days=2)
+    # end_date = date.today() + timedelta(days=2)
     uri = (
         "http://mesonet.agron.iastate.edu/cgi-bin/request/asos.py?"
-        f"station={station_id}&data=tmpc&year1=1928&month1=1&day1=1&"
-        f"year2={enddt.year}&month2={enddt.month}&day2={enddt.day}&"
+        f"station={station_id}&data=metar&"
+        f"year1={start_date.year}&month1={start_date.month}&day1={start_date.day}&"
+        f"year2={end_date.year}&month2={end_date.month}&day2={end_date.day}&"
         "tz=Etc%2FUTC&format=onlycomma&latlon=no&elev=no&missing=M&trace=T&"
         "direct=yes&report_type=3"
     )
     res = requests.get(uri, timeout=300)
-    with open(localfn, "w", encoding="utf-8") as fh:
+    with open(station_output_file, "w", encoding="utf-8") as fh:
         fh.write(res.text)
 
 
 def main():
     """Main loop."""
+    args = configargs()
+    validateargs(args)
+    station_id = args.station
+    start_date = datetime.strptime(args.start, "%Y%m%d")
+    end_date = datetime.strptime(args.end, "%Y%m%d")
+
+    # raise Exception(f"station_id: {station_id}, start_date: {start_date}, end_date: {end_date}")
+
+
     # Step 1: Fetch global METAR geojson metadata
     # https://mesonet.agron.iastate.edu/sites/networks.php
     req = requests.get(
@@ -46,20 +81,13 @@ def main():
     )
     geojson = req.json()
     for feature in geojson["features"]:
-        station_id = feature["id"]
-        props = feature["properties"]
-        # We want stations with data to today (archive_end is null)
-        if props["archive_end"] is None:
-            continue
-        # We want stations with data to at least 1943
-        if props["archive_begin"] is None:
-            continue
-        archive_begin = datetime.strptime(props["archive_begin"], "%Y-%m-%d")
-        if archive_begin.year > 1943:
-            continue
-        # Horray, fetch data
-        fetch(station_id)
-
+        if station_id == feature["id"]:
+            print(f"Found {station_id} in metadata")
+            props = feature["properties"]
+            # We want stations with data to today (archive_end is null)
+            if props["archive_end"] is None:
+                print(f"Station {station_id} has no archive_end")
+                fetch(station_id, start_date, end_date)
 
 if __name__ == "__main__":
     main()
