3090cd81a9ebf1b078f0f10ce530d6ff957e3c38
Jeffry Babb <ahuimanu@gmail.com>
2025-10-08 18:07:12 -0500

    fall2025

--- CuratedFlights/test.py
+++
@@ -1,8 +0,0 @@
-str_dep_time = str(0.0)
-str_dep_time = str_dep_time.strip()
-str_dep_time = str_dep_time.replace(".0", "")
-
-for i in range(4 - len(str_dep_time)):
-    str_dep_time = "0" + str_dep_time
-
-print(str_dep_time)
